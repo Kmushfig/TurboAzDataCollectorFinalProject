e5e6eb8b701b8fc5a74fd32c08cfcf83a1be6182
Professional <kazimovmu@gmail.com>
2023-02-03 20:27:49 +0400

    init

---
+++ src/main/java/com/finalproject/turboazdatacollector/TurboazDataCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject.turboazdatacollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurboazDataCollectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurboazDataCollectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/finalproject/turboazdatacollector/TurboazDataCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.turboazdatacollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurboazDataCollectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
