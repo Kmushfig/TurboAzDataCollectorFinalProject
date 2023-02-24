abfc2aa9a40391687630753793df123ff9346d89
Professional <kazimovmu@gmail.com>
2023-02-24 20:30:42 +0400

    First message

--- src/main/java/com/finalproject/turboazdatacollector/entity/CarsofEntity.java
+++ src/main/java/com/finalproject/turboazdatacollector/entity/CarsofEntity.java
@@ -1,29 +1,29 @@
-//package com.finalproject.turboazdatacollector.entity;
-//
-//import jakarta.persistence.*;
-//import lombok.*;
-//
-//@Entity
-//@Table(name = "turbo_cars")
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Builder
-//public class CarsofEntity {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private int id;
-//
-//    private String makeName;
-//    private String modelName;
-//    private int year;
-//    private int price;
-//    private int odometer;
-//    private double engine;
-//    private String dateTimeAndPlace;
-//
-//
-//
-//
-//}
+package com.finalproject.turboazdatacollector.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "turbo_cars")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CarsofEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String makeName;
+    private String modelName;
+    private int year;
+    private int price;
+    private int odometer;
+    private double engine;
+    private String dateTimeAndPlace;
+
+
+
+
+}
--- src/main/java/com/finalproject/turboazdatacollector/jSoup/DatasOfCarsJsoup.java
+++ src/main/java/com/finalproject/turboazdatacollector/jSoup/DatasOfCarsJsoup.java
@@ -1,6 +1,31 @@
-package com.finalproject.turboazdatacollector.jSoup;
-
-public class DatasOfCarsJsoup {
-
-
-}
+//package com.finalproject.turboazdatacollector.jSoup;
+//
+//import org.jsoup.Jsoup;
+//import org.jsoup.nodes.Document;
+//import org.jsoup.nodes.Element;
+//import org.jsoup.select.Elements;
+//
+//public class DatasOfCarsJsoup {
+//
+//    public void jsoupDatas
+//
+//    for (int j = 1; j < 2 ; j++) {
+//
+//    }
+//        Document doc = Jsoup.connect("https://turbo.az/autos?page=" + j).get();
+//        Elements productName = doc.getElementsByClass("products-i");
+//
+//        for (Element product : productName) {
+//
+//            Elements carName = product.getElementsByClass("products-i__name products-i__bottom-text");
+//            Elements attributes = product.getElementsByClass("products-i__attributes products-i__bottom-text");
+//            String[] atribittooSplittoo = attributes.get(0).text().split(", ");
+//            String year = atribittooSplittoo[0];
+//            String engine = atribittooSplittoo[1];
+//            String odoMetr = atribittooSplittoo[2];
+//            Elements price = product.getElementsByClass("products-i__price products-i__bottom-text");
+//            Elements dateTimeAndPlace = product.getElementsByClass("products-i__datetime");
+//
+//
+//
+//}
