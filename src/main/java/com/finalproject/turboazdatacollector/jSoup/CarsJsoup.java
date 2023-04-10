8ce277f3a8ac3d1d16f72f7034fa604901340c1f
Professional <kazimovmu@gmail.com>
2023-04-10 19:51:51 +0400

    last version 4

--- src/main/java/com/finalproject/turboazdatacollector/jSoup/CarsJsoup.java
+++ src/main/java/com/finalproject/turboazdatacollector/jSoup/CarsJsoup.java
@@ -2,30 +2,39 @@ package com.finalproject.turboazdatacollector.jSoup;
 
 import com.finalproject.turboazdatacollector.dtoCars.CarsDTO;
 import com.finalproject.turboazdatacollector.entity.CarsEntity;
+import com.finalproject.turboazdatacollector.entity.ModelEntity;
 import com.finalproject.turboazdatacollector.repository.CarsRepository;
+import com.finalproject.turboazdatacollector.repository.ModelRepository;
 import lombok.RequiredArgsConstructor;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 @Service
 @RequiredArgsConstructor
 public class CarsJsoup {
     private final CarsRepository carsRepository;
-
+    private final ModelRepository modelRepository;
     public CarsDTO jsoupDatas() throws IOException {
+
 
         CarsDTO carsDTO = new CarsDTO();
         HashMap<String, String> carIds = new HashMap<>();
+        ArrayList<ModelEntity> makeIdFromDB = modelRepository.findAllByMakeId();
+        ArrayList<ModelEntity> modelIdFromDB = modelRepository.findAllByModelId();
+        for (int i = 0; i < makeIdFromDB.size(); i++) {
+            for(int j = 0; j< modelIdFromDB.size();j++){
 
-        for (int j = 1; j < 2; j++) {
-
-            Document doc = Jsoup.connect("https://turbo.az/autos?page=" + j).get();
+            }
+        }
+            Document doc = Jsoup.connect("https://turbo.az/autos?q%5Bsort%5D=&q%5Bmake%5D%5B%5D=3&q%5Bmodel%5D%5B%5D=&q%5Bmodel%5D%5B%5D=35").get();
             Elements productName = doc.getElementsByClass("products-i");
 
             for (Element product : productName) {
@@ -62,8 +71,6 @@ public class CarsJsoup {
 
 
             }
-//
-        }
         return carsDTO;
     }
 
--- src/main/java/com/finalproject/turboazdatacollector/repository/ModelRepository.java
+++ src/main/java/com/finalproject/turboazdatacollector/repository/ModelRepository.java
@@ -1,6 +1,7 @@
 package com.finalproject.turboazdatacollector.repository;
 
 import com.finalproject.turboazdatacollector.entity.ModelEntity;
+import org.h2.engine.Mode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,6 @@ import java.util.ArrayList;
 public interface ModelRepository extends JpaRepository <ModelEntity, String>{
 //    ArrayList<ModelEntity>findAllByModelName(String modelName);
 //    ModelEntity findByModelName(String name);
+    ArrayList<ModelEntity> findAllByMakeId();
+    ArrayList<ModelEntity> findAllByModelId();
 }
